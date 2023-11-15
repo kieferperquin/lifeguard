daa542dbe53a963794fa25fbee2a7b3aebbdeeef
kieferperquin <91886036+kieferperquin@users.noreply.github.com>
2023-11-15 10:40:31 +0100

    debugging + small change in text objects

--- Assets/Scripts/Question/QuestionHandeler.cs
+++ Assets/Scripts/Question/QuestionHandeler.cs
@@ -94,19 +94,24 @@ public class QuestionHandeler : MonoBehaviour
 
     public void SetNextQuestion()
     {
-        QuestionsSO currentQuestionData = questionsDataArray[currentData];
-
         displayActivated = false;
 
         RemoveText();
 
-        SetQuestion(currentQuestionData);
+        SetQuestion(questionsDataArray[currentData]);
 
-        SetAnswers(currentQuestionData);
+        SetAnswers(questionsDataArray[currentData]);
 
-        currentData = currentData + 1;
+        currentData++;
 
-        debug.text = "currentData = " + currentData + ", maxDataAmount = " + maxDataAmount;
+        if (currentData == 1)
+        {
+            debug.text = "currentData = " + currentData;
+        }
+        else
+        {
+            debug.text = "currentData = " + currentData + " oegaboega";
+        }
     }
 
     void RemoveText()
@@ -154,8 +159,12 @@ public class QuestionHandeler : MonoBehaviour
     {
         int correctAnswer = Random.Range(1, 4);
 
+        debug.text = "made random going to make lists";
+
         correctListAmount = new List<int>();
         wrongListAmount = new List<int>();
+
+        debug.text = "made lists now adding shit to the lists";
 
         for (int i = 0; i < correctAnswerList.Count; i++)
         {
@@ -167,13 +176,22 @@ public class QuestionHandeler : MonoBehaviour
             wrongListAmount.Add(i);
         }
 
+        correctAnswer = 1;
+        debug.text = "added shit to the lists now selecting the random and setting the answers";
+
+
         if (correctAnswer == 1)
         {
+
             SetCorrectAnswer(answerText1, answerObject1, correctAnswerList);
+            debug.text = "correct answer has been set";
 
             SetWrongAnswer(answerText2, answerObject2, wrongAnswerList);
+            debug.text = "wrong answer 1 has been set";
             SetWrongAnswer(answerText3, answerObject3, wrongAnswerList);
+            debug.text = "wrong answer 2 has been set";
             SetWrongAnswer(answerText4, answerObject4, wrongAnswerList);
+            debug.text = "wrong answer 3 has been set";
         }
         else if (correctAnswer == 2)
         {
